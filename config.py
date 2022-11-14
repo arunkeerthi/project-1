894e317ec99e39311bd043ed58f403f063437a73
root <root@ip-172-31-11-105.ap-south-1.compute.internal>
2022-11-14 08:39:58 +0000

    project-1

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.crn75kxvhiw9.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "mybucket14nov"
+customregion = "ap-south-1"
